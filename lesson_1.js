427282abfdc989ba5599e257a58093632327457b
Dane knight <knight1seven@gmail.com>
2023-11-14 22:10:49 -0500

    javascript logical operators

--- lesson_1.js
+++ lesson_1.js
@@ -315,3 +315,237 @@ function reusableFunction() {
 reusableFunction();
 
 
+// Passing Values to Functions with Arguments
+function ourFunctionWithArgs(a, b) {
+    console.log(a -b);
+
+}
+ourFunctionWithArgs(10, 5);
+
+function functionWithArgs(a, b) {
+    console.log(a + b);
+}
+
+functionWithArgs(10, 5);
+
+//Global Scope and Functions
+
+var myGlobal = 10;
+
+function fun1() {
+    // Assign 5 to oopsGlobal Here
+    oopsGlobal = 5;
+}
+
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
+
+fun1();
+fun2();
+
+// Local Scope and Functions
+function myLocalScope() {
+    var myVar1 = 5;
+    console.log(myVar1);
+}
+
+myLocalScope();
+
+// Global vs Local Scope in functions
+
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+    var outerWear = "sweater"
+    return outerWear;
+}
+console.log(myOutfit());
+console.log(outerWear);
+
+// Return a value from a function with a return
+
+function minusSeven(num) {
+    return num -7;
+}
+
+console.log(minusSeven(10));
+
+function timesFive(num) {
+    return num * 5;
+}
+
+console.log(timesFive(5));
+
+// Understanding Undefined Value Returned from a function
+
+var sum = 0;
+function addThree() {
+    sum = sum + 3;
+}
+
+function addFive() {
+    sum += 5;
+}
+
+// Assignment with a Returned Value
+
+var changed = 0;
+
+function change(num) {
+    return (num + 5) / 3;
+
+}
+
+changed =  change(10);
+
+var processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+}
+
+processed = processArg(7);
+
+// Stand in Line
+function nextInLine(arr, item) {
+    
+    arr.push(item);
+    return arr.shift();
+}
+
+var testArr = [1,2,3,4,5];
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+//Boolean Values
+
+function welcomeToBooleans() {
+    return false;
+}
+
+//Using Conditional Logic with if Statements
+
+function ourTrueOrFalse(isItTrue) {
+    if (isItTrue) {
+        return "Yes, it's true";
+    }
+    return "No, its false";
+}
+
+function trueOrFalse(wasThatTrue) {
+if (wasThatTrue) {
+    return "Yes, that was true";
+    }
+    return "No, that was false";
+
+}
+
+console.log(trueOrFalse(true));
+
+// Comparison with the Equality Operator
+function testEqual(val) {
+    if (val == 12) {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+console.log(testEqual(10));  
+
+// Comparison with the Strict Equality Operator
+
+function testStrict(val) {
+    if (val === 7) {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+console.log(testStrict(10));
+
+// Practice Comparing Different Values
+// strict equality wil not convert the types 
+function compareEquality(a, b) {
+    if (a === b) {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+console.log(compareEquality(10, "10"));
+
+// Comparison with the Inequality operator
+
+function testNotEqual(val) {
+    if (val != 99) {
+        return "Not Equal";
+    }
+    return "Equal";
+}
+
+console.log(testNotEqual(10));
+
+// Comparison with the Strict Inequality operator
+
+function testStrictNotEqual(val) {
+
+    if (val !== 17) {
+
+        return "Not Equal";
+    }
+    return "Equal";
+}
+
+console.log(testStrictNotEqual(10));
+
+// Comparisons with the Greater than or Equal to Operator
+function testGreaterThan(val) {
+    if (val > 100) {
+        return "over 100";
+    }
+    if (val > 10) {
+        return "Over 10";
+    }
+    return "10 or Under";
+}
+
+console.log(testGreaterThan(10));
+
+function testGreaterorEqual(val) {
+    if (val >= 20) {
+        return "20 or Over";
+    }
+    if (val >= 10) {
+        return "10 or Over";
+    }
+    return "Less than 10";
+}
+
+console.log(testGreaterorEqual(10));
+
+// Comparisons with the Less than or Equal to Operator
+
+function testLessThan(val) {
+    if (val <= 20) {
+        return "under 20";
+    }
+    if  (val <= 50) {
+        return "under 50";
+    }
+    return "50 or over";
+}
+
+console.log(testLessThan(10));
+
+// Comparisons with the Logical "Or" Operator
+
