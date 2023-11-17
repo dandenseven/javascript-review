91f97102a4df53aac5e887f9f24f95b0995d00fc
Dane knight <knight1seven@gmail.com>
2023-11-16 21:28:52 -0500

    Javascript objects

--- lesson_1.js
+++ lesson_1.js
@@ -673,6 +673,212 @@ console.log(golfScore(5, 8));
 
 function caseInSwitch(val) {
     var answer = "";
+    switch(val) {
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    }
+
+    return answer;
 }
 
+console.log(caseInSwitch(1));
 
+// Default Options with Switch Statements
+function switchOfStuff(val) {
+    var answer = "";
+    switch (val) {
+        case "a":
+            answer = "apple";
+            break;
+        case "b":
+            answer = "bird";
+            break;
+        case "c":
+            answer = "cat";
+            break;
+        default:
+            answer = "stuff";
+            break;
+    }
+        return answer;
+}
+
+console.log(switchOfStuff("a"));
+
+//Multiple Identical Options in Switch Statements
+
+function sequentialSizes(val) {
+    var answer = "";
+    switch(val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+            break;
+        
+    }
+    return answer;
+}
+
+console.log(sequentialSizes(8));
+
+//Replace If Else Chains with Switch
+function chainToSwitch(val) {
+    var answer = "";
+
+//     if (val === "bob") {
+//         answer = "Marley";
+//     }   else if (val === 42) {
+//         answer = "The Answer";
+//     }   else if (val === 1) {
+//         answer = "There is no #1";
+//     }   else if (val === 99) {
+//         answer = "Missed me by this much!";
+//     }   else if (val === 7) {
+//         answer = "Ate Nine";
+//     }
+//     return answer;
+// }
+
+    switch (val) { 
+        case "bob":
+            answer = "Marley";
+            break;
+        case 42: 
+            answer = "The Answer";
+            break;
+        case 1:
+            answer = "There is no #1";
+            break;
+        case 99:
+            answer = "Missed me by this much!";
+            break;
+        case 7:
+            answer = "Ate Nine";
+            break;
+    }
+    return answer;
+}
+console.log(chainToSwitch())
+
+// Returning Boolean Values from functions
+
+function isLess(a, b) {
+    // fix this code
+    // if (a < b) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    return a < b;
+ }
+
+console.log(isLess(10, 15));
+
+// Returning Early Pattern from Functions
+
+function abTest(a, b) {
+
+    if (a < 0 || b < 0) {
+        return undefined;
+    }
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2))
+}
+
+console.log(abTest(-2 ,2));
+
+// Counting Cards Function
+
+var count = 0;
+
+function cc(card) {
+    switch(card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++;
+            break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--
+            break;
+    }
+
+    var holdbet = "Hold"
+    if (count > 0) {
+        holdbet = "Bet"
+    }
+    return count + " " + holdbet;
+}
+
+cc(2); cc(3); cc(7); cc("K"); cc("A");
+console.log(cc(10));
+
+// Build Javascript Objects
+// (properties) - "name": (values) "Camper"
+var ourDog = {
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything"]
+};
+
+
+var myDog = {
+    "name": "Quincy",
+    "legs": 3,
+    "tails": 2,
+    "friends": []
+
+
+}
+
+// Javascript Dot Notation
+
+var testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+var hatValue = testObj.hat;;
+var shirtValue = testObj.shirt;
+
+// Accessing Object Properties with Bracket Notation
+
+var testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink": "water"
+};
+
+var entreeValue = testObj["an entree"];
+var drinkValue = testObj["the drink"];
+
+// Accessing Object Properties with Variables
