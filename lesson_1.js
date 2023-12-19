f3f481e3dad7b356a7d410c3dbf17eae2500d326
Dane knight <knight1seven@gmail.com>
2023-12-18 22:06:39 -0500

    Destructuring Assignment with Rest Operator

--- lesson_1.js
+++ lesson_1.js
@@ -1577,24 +1577,116 @@ var x = voxel.x; // x = 3.6
 var y = voxel.y; // y = 7.4
 var z = voxel.z; // z = 6.54
 
-const { x : a, y : b, z : c } = voxel; // a = 3.6, b = 7.4, c = 6.54
+const { x : m, y : k, z : p } = voxel; // a = 3.6, b = 7.4, c = 6.54
 
 const AVG_TEMPERATURES = {
     today: 77.5,
     tomorrow: 79
 };
 
-function getTempofTmrw(avgTemperatures) {
+function getTempOfTmrw(avgTemperatures) {
     "use strict";
     
-    const { tomorrow : tempOFTomorrow } = avgTemperatures;
+    const { tomorrow : tempOfTomorrow } = avgTemperatures;
 
-    return tempOFTomorrow;
+    return tempOfTomorrow;
 }
 
 console.log(getTempOfTmrw(AVG_TEMPERATURES));
 
+// Destructing Assignment with Nested Objects
+// We can also use destructuring assignment to assign variables from nested objetcs
 
+const LOCAL_FOREST = {
+    today: { min: 72, max: 83 },
+    tomorrow: { min: 73.3, max: 84.6 }
+};
 
+function getMaxOfTmrw(forecast) {
+    "use strict";
 
+    const { tomorrow : { max : maxOfTomorrow }} = forecast;
 
+    return maxOfTomorrow;
+}
+
+console.log(getMaxOfTmrw(LOCAL_FOREST));
+
+// Use Destructuring Assignment to Assign Variables from Arrays
+
+const [l, q, , h] = [1, 2, 3, 4, 5, 6];
+console.log(l, q, h);
+
+let e = 8, f = 6;
+(() => {
+    "use strict";
+    [e, f]= [f, e]
+
+})();
+console.log(e);
+console.log(f);
+
+// Use Destructuring Assignment with the Rest Operator
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+    
+    const [ , , ...arr] = list;
+
+    return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr);
+console.log(source);
+
+// Use Destructuring Assignment to Pass an Object as a Function's Parameters
+// good to use with API calls
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+
+const half = (function() {
+
+    return function half({max, min}) {
+        return (max + min) / 2.0;
+    };
+
+})();
+console.log(stats);
+console.log(half(stats));
+
+// Create Strings using Template Literals
+
+const person = {
+    name: "Zodiac Hasbro",
+    age: 56
+}
+
+//Template literal with multi-line and string interpolation
+const greeting = `Hello, my name is ${person.name}!
+I am ${person.age} years old.`;
+
+console.log(greeting);
+
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreaker"],
+    skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+    const resultDisplayArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        resultDisplayArray.push(`<li class="text-warning"> ${arr[i]}</li>`)
+    }
+
+    return resultDisplayArray
+}
+
+const resultDisplayArray = makeList(result.failure);
+
+console.log(resultDisplayArray)
